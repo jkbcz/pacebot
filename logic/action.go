970f85e23e0e8a7fe1d711ccb549ed6923818652
Jakub Czyż <me@jakubc.pl>
2026-06-15 18:56:16 +0200

    fix last milestone

--- logic/action.go
+++ logic/action.go
@@ -17,7 +17,7 @@ var actionMilestones = []Milestone{
 	{Date: lo.Must(time.Parse(time.RFC3339, "2026-04-01T00:00:00Z")), Milestone: 33},
 	{Date: lo.Must(time.Parse(time.RFC3339, "2026-05-01T00:00:00Z")), Milestone: 44},
 	{Date: lo.Must(time.Parse(time.RFC3339, "2026-06-01T00:00:00Z")), Milestone: 55},
-	{Date: lo.Must(time.Parse(time.RFC3339, "2025-07-01T00:00:00Z")), Milestone: 66},
+	{Date: lo.Must(time.Parse(time.RFC3339, "2026-07-01T00:00:00Z")), Milestone: 66},
 }
 
 func getStatusForNextMilestone(now time.Time) float64 {
